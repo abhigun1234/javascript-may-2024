643f1bb19c05c535f1465c0706562f3c85d9e140
unknown <abhi.ghosh27feb@gmail.com>
2024-05-25 12:14:25 +0530

    add

---
+++ second.js
@@ -0,0 +1,26 @@
+// data type
+// primitive type
+// number 
+
+var a=10;
+
+console.log("type of a ",typeof(a))
+var str='abhi'
+console.log("type of str ",typeof(str))
+
+// undefined and null
+
+var c;
+console.log("type of c ",c,typeof(c))
+var d=null;
+console.log("type of d ",d,typeof(d))
+// differnce between undefined and null 
+
+// boolen 
+
+var e=true;
+var k=false;
+
+
+
+
---
+++ third.js
@@ -0,0 +1,84 @@
+// var no1=Number(prompt("enter first no"))
+// var no2=Number(prompt("enter second no"))
+// result=no1+no2
+// console.log("result",result)
+
+// conditional operators
+// == === >= <= != ,
+
+
+var a=20;
+var b=11;
+// console.log("a>b",a>b)
+// console.log("a<b",a<b)
+// console.log("a!=b",a!=b)
+// console.log("a==b",a==b)
+// console.log("a===b",a===b)
+// // conditional statement
+
+// if(a===b){
+//     console.log("a and b are equal")
+// }
+// else if(a>b){
+//     console.log("a is greater ")
+// }
+// else{
+//     console.log("a and b are not equal")
+// }
+// // input the age of user from prompt 
+// // if user age <5 then provide 50% of the discount on the ticket price
+// // if age is in between 5 and 55 then provide 30% of the discunt on the  ticket price
+// // if age is > 55 provide the discount of 25 %
+
+// // ticket price is 5000
+
+// // loops
+
+// // while 
+// // do while 
+// // for loop
+// var i=0;
+// while(i<10){
+
+//     console.log("i",i)
+//     i++// post increment (i=i+1)
+// }
+
+// console.log("i==",i)
+
+// // do while
+// var j=0
+// do{
+
+// j++
+// console.log("j==",j)
+// }while(j<10)
+// problems  
+
+// enter a no from prompt and print the table of that no
+// check the no is prime or no
+// enter a string and reverse the string and check it is pelendrom or not
+// 0,1,1,2,3 fabonocee series
+// print below peramid
+// *
+// * *
+// * * * 
+// * * *
+
+// for loop 
+// 
+// for( var i=0;i<10;i++){
+
+//     console.log("i==",i)
+// }
+console.log("i==",i)
+// let and var
+
+// const 
+const l=0;
+l=12
+
+
+
+
+
